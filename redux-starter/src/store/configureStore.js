a16de2b1e90297a5e13c4a7e70d485410e91fc92
bethanyann <bethanyann44@gmail.com>
2022-11-29 11:07:33 -0600

    convert create store function to redux toolkit configureStore

--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -1,11 +1,18 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+import reducer from './bugs';
 
 //createStore is a good example of a higher-order function bc it takes a function (reducer) as an argument
 
 //then export the store so it can be used in the main application
-export default function configureStore() {
-    const store = createStore(reducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
-    );
+export default function createStore() {
+    // const store = createStore(reducer,
+    //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
+    // );
+
+    //create the redux toolkit store and pass it a configuration object 
+    const store = configureStore({
+        reducer: reducer
+    });
     return store;
 }
