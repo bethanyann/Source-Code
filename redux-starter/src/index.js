a77bbbbb8940eeade1783a9e77d76f4a70920b4c
bethanyann <bethanyann44@gmail.com>
2022-11-30 21:22:28 -0600

    converting redux reducers to redux toolkit reducers

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -2,20 +2,21 @@ import configureStore from './store/configureStore';
 import { bugAdded, bugResolved } from './store/bugs';
 
 const store = configureStore();
-//subscribe to the store
-//this function returns a method for unsubscribing to the store 
+// subscribe to the store
+// this function returns a method for unsubscribing to the store 
 const unsubscribe = store.subscribe(() => {
-    //this gets called anytime there are changes to the state in the store
-    //this is where the UI will subscribe to the store so that the UI gets notified when the state changes
-    //and can update
+    // this gets called anytime there are changes to the state in the store
+    // this is where the UI will subscribe to the store so that the UI gets notified when the state changes
+    // and can update
     console.log("Store changed!");
 });
 
-//easier way to dispatch an action is to call the action creator like this: 
-store.dispatch(bugAdded("Bug 1"));
-store.dispatch(bugAdded("Bug 2"));
-store.dispatch(bugAdded("Bug 3"));
-store.dispatch(bugResolved(1));
+// easier way to dispatch an action is to call the action creator like this: 
+// wrap the string value in an object with the description property for redux toolkit -- need to do it like this so the reducer doesn't break
+store.dispatch(bugAdded({ description: "Bug 1" })); 
+store.dispatch(bugAdded({ description: "Bug 2" }));
+store.dispatch(bugAdded({ description: "Bug 3" }));
+store.dispatch(bugResolved({ id: 1}));
 
 console.log(store);
 
--- redux-starter/src/store/bugs.js
+++ redux-starter/src/store/bugs.js
@@ -1,26 +1,33 @@
 
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
 // ACTION TYPES
 // store all of the different action type strings in one place to reduce code duplication 
-const BUG_ADDED = "BUG_ADDED";
-const BUG_REMOVED = "BUG_REMOVED";
-const BUG_RESOLVED = "BUG_RESOLVED";
+// These are no longer needed with the addition of Redux Toolkit
+// const BUG_ADDED = "BUG_ADDED";
+// const BUG_REMOVED = "BUG_REMOVED";
+// const BUG_RESOLVED = "BUG_RESOLVED";
 
 // ACTION CREATORS
+//Redux Toolkit way of writing Action Creators
+export const bugAdded = createAction("bugAdded");
+export const bugResolved = createAction("bugResolved");
+export const bugRemoved = createAction("bugRemoved");
 
-export const bugAdded = description => ({
-    type: BUG_ADDED,
-    payload: {
-        description: description
-    }
-});
+//now you can go bugAdded.type and get the action type from that, so the constants defined above are no longer needed
 
-export const bugResolved = id => ({
-    type: BUG_RESOLVED,
-    payload: {
-        id: id
-    }
-});
-
+// export const bugAdded = description => ({
+//     type: BUG_ADDED,
+//     payload: {
+//         description: description
+//     }
+// });
+// export const bugResolved = id => ({
+//     type: BUG_RESOLVED,
+//     payload: {
+//         id: id
+//     }
+// });
 // action creator as not an arrow function
 // export function bugAdded(description) {
 //     //dispatch an action 
@@ -36,32 +43,51 @@ export const bugResolved = id => ({
 
 let lastId = 0;
 
+// Redux Toolkit Reducer - the first argument is the initial state, the second argument is a function that maps actions to functions that handle the actions
+export default createReducer([], { 
+    // key: name of the action
+    // actions: functions --> kind of like an event being mapped to an event handler
+    [bugAdded.type]: (state, action) => {
+        //write mutating code here - what changes when this action happens
+        state.push({
+            id: ++lastId,
+            description: action.payload.description,
+            resolved: false
+        });
+    },
+    [bugResolved.type]: (state, action) => {
+       const index = state.findIndex(bug => bug.id === action.payload.id);
+       state[index].resolved = true;
+    },
+    [bugRemoved.type]: (state, action) => {
+        state.filter(bug => bug.id !== action.payload.id)
+    }
+})
 //on first load, state will be undefined as nothing has been set yet.
 //to prevent application from blowing up, set state = [] to represent the initial state
 
-export default function reducer(state = [], action) {
-    //can also implement this logic with a switch/case structure
-    if(action.type === BUG_ADDED) {
-        //return array of new bug object
-        return [
-            ...state,
-            {
-                description: action.payload.description,
-                resolved: false,
-                id: ++lastId
-            }
-        ]
-    } 
-    else if(action.type === BUG_REMOVED) {
-        return state.filter(bug => bug.id !== action.payload.id)
-    }
-    else if(action.type === BUG_RESOLVED) {
-        debugger;
-        return state.map(bug => 
-            bug.id == action.payload.id ? {...bug, resolved: true} : bug  //if the bug id matches, return a new object by copying the bug and then setting the resolved property to true
-        )
-    }
-    //very important to return the current state if there is an action type that isn't handled, so that the application 
-    //doesnt blow up
-    else return state;
-}
+// export default function reducer(state = [], action) {
+//     //can also implement this logic with a switch/case structure
+//     if(action.type === bugAdded.type) {
+//         //return array of new bug object
+//         return [
+//             ...state,
+//             {
+//                 description: action.payload.description,
+//                 resolved: false,
+//                 id: ++lastId
+//             }
+//         ]
+//     } 
+//     else if(action.type === bugRemoved.type) {
+//         return state.filter(bug => bug.id !== action.payload.id)
+//     }
+//     else if(action.type === bugResolved.type) {
+//         return state.map(bug => 
+//             bug.id == action.payload.id ? {...bug, resolved: true} : bug  //if the bug id matches, return a new object by copying the bug and then setting the resolved property to true
+//         )
+//     }
+//     //very important to return the current state if there is an action type that isn't handled, so that the application 
+//     //doesnt blow up
+//     else return state;
+// }
