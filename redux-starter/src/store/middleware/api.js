9ca5dbabfedb91b2133d2175f82a30d32265e67f
bethanyann <bethanyann44@gmail.com>
2022-12-07 22:24:36 -0600

    adding axios for api calls

--- redux-starter/src/store/middleware/api.js
+++ redux-starter/src/store/middleware/api.js
@@ -1,11 +1,22 @@
 
-//example of currying here
+//define specific types of actions that deal with api calls
 const api = store => next => action => {
-    // type: 'apiCallBegin',
-    // payload: {
-    //     url: '/bugs',
-    //     method: 'get'
-    // }
+   if(action.type !== 'apiCallBegan') {
+    //if action does not match, pass it to the next one in the line
+    next(action);
+    return;
+   }
 };
+
+const action = {
+    type: 'apiCallBegan',
+    payload: {
+        url: '/bugs',
+        method: 'get',
+        data: {},
+        onSuccess: 'bugsReceived', // using strings, not passing functions as callbacks because this action should be serializable
+        onError: 'apiRequestFailed' // functions are not serializable
+    }
+}
 
 export default api;
