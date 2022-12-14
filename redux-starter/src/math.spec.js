adf8f5398c621b7b9f0aacc056ee20a0f0c08d0b
bethanyann <bethanyann44@gmail.com>
2022-12-13 21:07:48 -0600

    removing package.json outside of project folder

---
+++ redux-starter/src/math.spec.js
@@ -0,0 +1,22 @@
+import { isEven }from './math';
+
+//define a group or suite of tests 
+describe("isEven function testing", () => {
+    //define name of test and then define function that runs as the test 
+    it("isEven should return true if given an even number", () => {
+        //function under test
+        const result = isEven(2);
+
+        //make an assertion as to what you expect to happen
+        expect(result).toEqual(true);
+    });
+
+    it("isEven should return false if given an odd number", () => {
+        //function under test
+        const result = isEven(1);
+
+        //make an assertion as to what you expect to happen
+        expect(result).toEqual(false);
+    });
+});
+
